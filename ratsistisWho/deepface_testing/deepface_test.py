4bf87879da6d591099dbc4a3b16c8a93b0d224af
papakitotrelo <49374595+papakitotrelo@users.noreply.github.com>
2023-05-05 02:34:48 +0300

    Update deepface_test.py

--- ratsistisWho/deepface_testing/deepface_test.py
+++ ratsistisWho/deepface_testing/deepface_test.py
@@ -1,5 +1,5 @@
 from deepface import DeepFace
 
-obj = DeepFace.analyze(img_path='panemorfosMixalis.jpg', actions = ['race'])
+obj = DeepFace.analyze(img_path='beautifulPaidi.jpg', actions = ['race'])
 
 print(obj)
